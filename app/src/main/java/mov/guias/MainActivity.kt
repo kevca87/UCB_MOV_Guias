a1c7b16d072dc5b38cbb7ca9b2bfa87bbe2abb92
Kevin <kevcespedesarancibia87@gmail.com>
2023-03-02 23:33:56 -0400

    GUIA3 DONE: Added buttons for bckgnd color change

--- app/src/main/java/mov/guias/MainActivity.kt
+++ app/src/main/java/mov/guias/MainActivity.kt
@@ -1,11 +1,27 @@
 package mov.guias
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+    private val btnLight: Button
+        get() = findViewById(R.id.btnLight)
+    private val btnDark: Button
+        get() = findViewById(R.id.btnDark)
+    private val layoutPrincipal: ConstraintLayout
+        get() = findViewById(R.id.layoutPrincipal)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDark.setOnClickListener{
+            layoutPrincipal.setBackgroundColor(Color.DKGRAY)
+        }
+        btnLight.setOnClickListener{
+            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        }
     }
 }
