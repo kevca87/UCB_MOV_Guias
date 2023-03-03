859459e101fb07f229920b44f83fc5262426b95d
Kevin <kevcespedesarancibia87@gmail.com>
2023-03-03 00:25:12 -0400

    GUIA5: Menu and icon added

--- app/src/main/java/mov/guias/MainActivity.kt
+++ app/src/main/java/mov/guias/MainActivity.kt
@@ -3,6 +3,9 @@ package mov.guias
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,19 @@ class MainActivity : AppCompatActivity() {
         supportActionBar.let {
             setSupportActionBar(toolBar)
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.menu_item1 -> {
+                Log.d("TEST", "PRESS ICON MENU")
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
