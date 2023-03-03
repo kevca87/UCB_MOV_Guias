7f64a9485b2dfb156b96e3dbd0b68719c17c2fcd
Kevin <kevcespedesarancibia87@gmail.com>
2023-03-02 23:51:46 -0400

    GUIA4: tool bar added

--- app/src/main/java/mov/guias/MainActivity.kt
+++ app/src/main/java/mov/guias/MainActivity.kt
@@ -1,11 +1,19 @@
 package mov.guias
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
 class MainActivity : AppCompatActivity() {
+    private val toolBar: Toolbar
+        get() = findViewById(R.id.toolbar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar.let {
+            setSupportActionBar(toolBar)
+        }
     }
 }
